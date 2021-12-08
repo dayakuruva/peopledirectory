fbf3a1d97fc7d2cc5a3b9aeb49c1952c9f1f4b5b
dayasudhan <dayasudhankg@gmail.com>
2021-12-08 12:26:10 +0530

    nestjsrest

---
+++ src/app.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get ,Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('daya')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('kg')
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('kgg')
+  getHello2(): string {
+    return this.appService.getHello();
+  }
+  @Get()
+  getHello3(): string {
+    return "this.appService.getHello()";
+  }
+
+  @Post()
+  insertHello(@Body('name') name:string):string{
+    console.log(name)
+    return name;
+  }
+ 
+ 
+}
